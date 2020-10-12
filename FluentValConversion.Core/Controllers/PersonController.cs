278d1ddb253a5ef9356b5de3c0178e6ae6638cc1
Afroze Amjad <afrozeamjad@gmail.com>
2020-10-12 23:31:00 +0500

    Converted lib to netstandard 2.0 Added core 3.1 project

---
+++ FluentValConversion.Core/Controllers/PersonController.cs
@@ -0,0 +1,34 @@
+﻿using FluentValConversion.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FluentValConversion.Core.Controllers
+{
+    public class PersonController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Person person)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Create", person);
+            }
+
+            TempData["notice"] = "Success";
+            return RedirectToAction("Index");
+        }
+    }
+}
